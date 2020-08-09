5648e4fb0af33d5768a500aeb1f050b4880e3322
Tomas Gil <tomasgil1234@gmail.com>
2020-08-09 18:58:12 -0300

    feat: setup app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 
+// eslint-disable-next-line react/prop-types
 function MyApp({ Component, pageProps }) {
+  // eslint-disable-next-line react/jsx-props-no-spreading
   return <Component {...pageProps} />
 }
 
