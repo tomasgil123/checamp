ed356894c6a3819e1dccd3cb8f0de37bba58c8ca
Tomas Gil <tomasgil1234@gmail.com>
2020-09-06 12:33:23 -0300

    refactor: how it owrks section

--- src/components/home/howItWorks/index.js
+++ src/components/home/howItWorks/index.js
@@ -13,8 +13,6 @@ const Container = styled.div`
 
 const ContainerItems = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
   flex-direction: column;
   margin: auto;
   @media (min-width: ${breakpoints.md}) {
@@ -35,22 +33,40 @@ const Title = styled.div`
   }
 `
 
+const TextBody = styled.div`
+  color: ${colors.text.secondary};
+  font-size: ${space.s4};
+  font-weight: 400;
+  width: 70%;
+  margin: auto;
+  text-align: center;
+  padding-bottom: ${space.s4};
+  @media (min-width: ${breakpoints.md}) {
+    font-size: ${space.s5};
+  }
+`
+
 function HowItWorks() {
   return (
     <Container>
       <Title>¿Cómo funciona?</Title>
+      <TextBody>
+        Checamp es el lugar de encuentro para aquellas personas que tienen una casa rodante que
+        quieren alquilar y personas que buscan una casa rodante para sus próximas vacaciones
+      </TextBody>
       <ContainerItems>
         <Item
-          text="Contratamos seguros especiales para cada una de las autocaravanas que se alquilan en nuestra plataforma"
-          icon="https://d1o5877uy6tsnd.cloudfront.net/marketplace-search-production/images/icons/insurance-de28d3957af71938dd1dde41c4a3dbd0.svg"
+          text="Publica tu casa rodante"
+          topPx="-18px"
+          icon="https://d1o5877uy6tsnd.cloudfront.net/marketplace-search-production/images/icons/delivery-4db5ab4a99fe3e47cd538189615dd9db.svg"
         />
         <Item
-          text="Todos los usuarios de la pagina antes de poder alquilar un autocaravana deben pasar una serie de controles estrictos"
+          text="Recibi contactos de personas interesadas en alquilarla"
+          icon="https://d1o5877uy6tsnd.cloudfront.net/marketplace-search-production/images/icons/dmv-positive-d837b0e827b93ac3f8a0ae0d03887ac9.svg"
+        />
+        <Item
+          text="Alquila tu casa rodante y gana plata mientras no la usas"
           icon="https://d1o5877uy6tsnd.cloudfront.net/marketplace-search-production/images/icons/reviews-big-f2783008163826c3318785d4eb0054af.svg"
-        />
-        <Item
-          text="Tenemos un equipo de soporte listo para ayudarte en lo que necesites"
-          icon="https://d1o5877uy6tsnd.cloudfront.net/marketplace-search-production/images/icons/support-9f0b0a9ecca1f83e11baaa77b67e878c.svg"
         />
       </ContainerItems>
     </Container>
