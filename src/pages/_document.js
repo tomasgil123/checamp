83bee4584cf02b18dd1c24cc09e6f1ddd1b0913a
Tomas Gil <tomasgil1234@gmail.com>
2020-09-20 20:00:20 -0300

    fix: tag manager

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -40,7 +40,7 @@ export default class MyDocument extends Document {
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '&gtm_auth=BCxU5QaddBS7cH89ieDj1A&gtm_preview=env-3&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${process.env.TM_TRACKING_ID}');}`,
+              })(window,document,'script','dataLayer','${process.env.TM_TRACKING_ID}');`,
             }}
           />
         </Head>
