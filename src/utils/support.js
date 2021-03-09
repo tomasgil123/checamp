f21749e9ef4bb48da735aeb3999622f148dcc967
Tomas Gil <tomasgil1234@gmail.com>
2021-03-09 08:10:06 -0300

    refactor: change support number

--- src/utils/support.js
+++ src/utils/support.js
@@ -1,8 +1,7 @@
 const support = {
-  supportNumber: '291-4484082',
-  supportNumberComplete: '5492914484082',
+  supportNumber: '11-5621-7620',
+  supportNumberComplete: '5491156217620',
   allSupportNumbers: [
-    { name: 'Guido', number: '291-4484082', numberComplete: '5492914484082' },
     { name: 'Agus', number: '11-6966-4664', numberComplete: '5491169664664' },
     { name: 'Tomi', number: '11-5621-7620', numberComplete: '5491156217620' },
   ],
