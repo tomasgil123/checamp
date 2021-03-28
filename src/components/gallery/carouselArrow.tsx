02714d1c9a2e7b7a562ce8b9126a21917d826a26
Tomas Gil <tomasgil1234@gmail.com>
2021-03-28 16:58:14 -0300

    fix: build issue

--- src/components/gallery/carouselArrow.tsx
+++ src/components/gallery/carouselArrow.tsx
@@ -1,20 +1,21 @@
-import { RenderArrowProps } from 'react-elastic-carousel'
+// import { RenderArrowProps } from 'react-elastic-carousel'
 
-const CarouselArrow = ({ type, onClick }: RenderArrowProps): JSX.Element => {
-  const isPrev = type === 'PREV'
-  const arrowClassName = cx('cursor-pointer z-10 absolute bg-white hidden rounded-sm top-2/4', {
-    [s.prev]: isPrev,
-    [s.next]: !isPrev,
-  })
-  const img = {
-    className: arrowClassName,
-    src: `/static/img/blue-arrow-${isPrev ? 'prev' : 'next'}.svg`,
-  }
-  return (
-    <button onClick={onClick} tabIndex={0} aria-label={isPrev ? 'prev' : 'next'}>
-      <img {...img} alt={type} />
-    </button>
-  )
-}
+// const CarouselArrow = ({ type, onClick }: RenderArrowProps): JSX.Element => {
+//   const isPrev = type === 'PREV'
+//   const arrowClassName = cx('cursor-pointer z-10 absolute bg-white hidden rounded-sm top-2/4', {
+//     [s.prev]: isPrev,
+//     [s.next]: !isPrev,
+//   })
+//   const img = {
+//     className: arrowClassName,
+//     src: `/static/img/blue-arrow-${isPrev ? 'prev' : 'next'}.svg`,
+//   }
+//   return (
+//     <button onClick={onClick} tabIndex={0} aria-label={isPrev ? 'prev' : 'next'}>
+//       <img {...img} alt={type} />
+//     </button>
+//   )
+// }
 
-export default CarouselArrow
+// export default CarouselArrow
+export {}
