2b1ed2b18c7b261232dfb4a858578c630b952b71
Tomas Gil <tomasgil1234@gmail.com>
2021-03-31 20:19:11 -0300

    fix: footer position issue

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,12 +1,12 @@
 import Header from 'src/components/header'
 import Footer from 'src/components/footer'
-import { useRef, useEffect, useState } from 'react'
+import { useRef, useLayoutEffect, useState } from 'react'
 
 // eslint-disable-next-line react/prop-types
 export default function Layout({ children }) {
   const containerPage = useRef()
   const [addPositionAbsolute, setAddPositionAbsolute] = useState(false)
-  useEffect(() => {
+  useLayoutEffect(() => {
     if (containerPage && typeof window !== 'undefined') {
       //we add 150 because that is aproximately the footers height
       setAddPositionAbsolute(containerPage.current.clientHeight + 150 < window.innerHeight)
