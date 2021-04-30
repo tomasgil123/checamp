b33c97fa407fa03de9b9c470cc913023619b3e5b
Tomas Gil <tomasgil1234@gmail.com>
2021-04-30 20:12:14 -0300

    refactor: style modals

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -24,7 +24,7 @@ const Modal = ({ children, showModal, closeModal }: IncomingProps): JSX.Element 
       isOpen={showModal}
       onDismiss={closeModal}
       aria-label="dialog"
-      className="bg-black bg-opacity-30 shadow-2xl fixed top-0 right-0 bottom-0 left-0 z-10"
+      className="fixed bg-black bg-opacity-30 shadow-2xl top-0 right-0 bottom-0 left-0 z-10 overflow-y-scroll"
     >
       <AnimatePresence>
         <motion.div
@@ -37,11 +37,11 @@ const Modal = ({ children, showModal, closeModal }: IncomingProps): JSX.Element 
           }}
         >
           <DialogContent aria-label="dialog" className="m-auto md:mt-11 focus:outline-none">
-            <div className="relative md:h-1/2 mt-20 bg-white w-full rounded-sm max-w-screen-md m-auto">
+            <div className="relative mt-20 bg-white w-full rounded-sm max-w-screen-md m-auto">
               <div className="flex justify-end w-full pt-3 px-3 sticky top-0 bg-white">
                 <img className="h-4 w-4 cursor-pointer" onClick={closeModal} src={closeIcon} />
               </div>
-              <div className="overflow-y-auto h-screen pb-8">{children}</div>
+              <div className="pb-8">{children}</div>
             </div>
           </DialogContent>
         </motion.div>
