d279987f08c0ccb3fead026767af1ae9535f1910
Tomas Gil <tomasgil1234@gmail.com>
2021-05-17 23:19:52 -0300

    fix: fix purge tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 module.exports = {
-  purge: [],
+  purge: ['src/pages/**/*.{js,ts,jsx,tsx}', 'src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   variants: {
     extend: {},
