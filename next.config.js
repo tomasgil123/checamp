0d11facb70dced1e672ac55af8e2747fa534cf4f
Tomas Gil <tomasgil1234@gmail.com>
2021-05-17 22:55:23 -0300

    feat: add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,14 +1,19 @@
 const withOptimizedImages = require('next-optimized-images')
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 const path = require('path')
 
-module.exports = withOptimizedImages({
-  webpack(config) {
-    // eslint-disable-next-line no-param-reassign
-    config.resolve.alias.images = path.join(__dirname, 'src/assets/images')
-    return config
-  },
-  images: {
-    domains: ['res.cloudinary.com'],
-  },
-})
+module.exports = withBundleAnalyzer(
+  withOptimizedImages({
+    webpack(config) {
+      // eslint-disable-next-line no-param-reassign
+      config.resolve.alias.images = path.join(__dirname, 'src/assets/images')
+      return config
+    },
+    images: {
+      domains: ['res.cloudinary.com'],
+    },
+  })
+)
